9183efb55522a37f287f14a1542d42e088bf3a10
iammephisto <jmdesrosiers@gmail.com>
2018-06-28 15:04:51 -0400

    Removed target folder

--- control_flow/target/debug/control_flow.d
+++
@@ -1,1 +0,0 @@
-/home/mephisto/projects/control_flow/target/debug/control_flow: /home/mephisto/projects/control_flow/src/main.rs
--- control_flow/target/debug/deps/control_flow-64caa178ee274f37.d
+++
@@ -1,5 +0,0 @@
-/home/mephisto/projects/control_flow/target/debug/deps/control_flow-64caa178ee274f37: src/main.rs
-
-/home/mephisto/projects/control_flow/target/debug/deps/control_flow-64caa178ee274f37.d: src/main.rs
-
-src/main.rs:
