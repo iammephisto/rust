e8abb917515b39fac5dcbaa3bc6099cbe30ecf8f
iammephisto <jmdesrosiers@gmail.com>
2018-06-28 14:41:23 -0400

    Finished functions part of the Rust Book

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+// This program demonstrates rust function
+// Written by iammephisto@github / VIM / Kali_WSL
 
-    another_function(5);
+fn main() {
+    println!("Function runs another function that prints a value.");
+    another_function(5, 6);
+    println!("Function returns 5");
+    let x = five();
+    println!("The value of x is: {}", x);
+    println!("Functions return x+1");
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
 }
 
-fn another_function(x: i32){
-    println!("Another function with value {}", x);
+fn plus_one(x: i32) -> i32 {
+    x+1
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn another_function(x: i32, y: i32){
+    println!("Another function with value {}, {}", x, y);
 }
