b416d6ed1919a39abf2bc1b7c46e3981fa82161c
iammephisto <jmdesrosiers@gmail.com>
2018-07-29 00:15:27 -0400

    Communicator

--- chapter6_1/target/debug/chapter6_1.d
+++ chapter6_1/target/debug/chapter6_1.d
@@ -1,1 +1,1 @@
-/home/ubuntu/workspace/dev/rust/chapter6_1/target/debug/chapter6_1: /home/ubuntu/workspace/dev/rust/chapter6_1/src/main.rs
+/home/mephisto/projects/chapter6_1/target/debug/chapter6_1: /home/mephisto/projects/chapter6_1/src/main.rs
---
+++ chapter6_1/target/debug/deps/chapter6_1-71c17f359e1b5231.d
@@ -0,0 +1,5 @@
+/home/mephisto/projects/chapter6_1/target/debug/deps/chapter6_1-71c17f359e1b5231: src/main.rs
+
+/home/mephisto/projects/chapter6_1/target/debug/deps/chapter6_1-71c17f359e1b5231.d: src/main.rs
+
+src/main.rs:
